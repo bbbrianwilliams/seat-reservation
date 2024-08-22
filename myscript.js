7626ace1f99a8805f4a049e4352b58873569c238
Brian Williams <120751965+bbbrianwilliams@users.noreply.github.com>
2024-08-22 10:52:59 -0400

    initial layout

---
+++ myscript.js
@@ -0,0 +1,56 @@
+const rows = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+];
+
+let html = "";
+let counter = 1;
+
+rows.forEach(function (row) {
+  html += `<div class="label">${row}</div>`;
+
+  for (let i = 0; i < 3; i++) {
+    html += `<div id="${row + counter}">${counter}</div>`;
+    counter++;
+  }
+  counter = counter + 12;
+});
+
+document.getElementById("left").innerHTML = html;
+
+html = "";
+counter = 1;
+
+rows.forEach(function (row) {
+  counter = counter + 12;
+
+  for (let i = 0; i < 3; i++) {
+    html += `<div id="${row + counter}">${counter}</div>`;
+    counter++;
+  }
+
+  html += `<div class="label">${row}</div>`;
+});
+
+document.getElementById("right").innerHTML = html;
+
+html = "";
+counter = 1;
